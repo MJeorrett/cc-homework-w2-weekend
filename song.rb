b55333bdcc9896aad4ef889ef52c9edb949aa9a6
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-07 20:47:42 +0100

    other fiels changed when adding release year to song

--- song.rb
+++ song.rb
@@ -2,12 +2,13 @@ require_relative('record')
 
 class Song < Record
 
-  attr_reader :title, :artist
+  attr_reader :title, :artist, :release_year
 
-  def initialize(title, artist)
+  def initialize(title, artist, release_year)
 
     @title = title
     @artist = artist
+    @release_year = release_year
 
   end
 
