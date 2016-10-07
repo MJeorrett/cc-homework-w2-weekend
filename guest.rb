f8ff945c42e829d3c5a1d803cabe3a9bff1a2a6a
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-07 18:56:06 +0100

    rest of files affected by removing guest id

--- guest.rb
+++ guest.rb
@@ -2,12 +2,11 @@ require_relative('record')
 
 class Guest < Record
 
-  attr_reader :first_name, :last_name, :id
+  attr_reader :first_name, :last_name
 
-  def initialize(first_name, last_name, id)
+  def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    @id = id
   end
 
   def full_name()
--- guest_manager.rb
+++ guest_manager.rb
@@ -5,25 +5,16 @@ class GuestManager
   attr_reader :guests
 
   def initialize
-    @next_guest_id = 0
     @guests = []
   end
 
   def add_guest(first_name, last_name)
-    new_guest = Guest.new(first_name, last_name, get_next_guest_id())
+    new_guest = Guest.new(first_name, last_name)
     @guests.push(new_guest)
   end
 
   def has_guest(guest)
     return @guests.include?(guest)
-  end
-
-  private
-
-  def get_next_guest_id()
-    id = @next_guest_id
-    @next_guest_id += 1
-    return id
   end
 
 end
