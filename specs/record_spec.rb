85ec0aa93e511a6672c224d48d51627a8a4b0040
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-07 17:10:06 +0100

    changed tests to use Song class as it doesn't have a unique id

--- specs/record_spec.rb
+++ specs/record_spec.rb
@@ -1,74 +1,74 @@
 require('minitest/autorun')
 require('minitest/rg')
-require_relative('../guest')
-require_relative('../guest2')
+require_relative('../song')
+require_relative('../song2')
 
 require('pry-byebug')
 
 class RecordTest < MiniTest::Test
 
   def setup()
-    @guest_joe_elder = Guest.new("Joe", "Elder")
-    @guest_joe_danger_1 = Guest.new("Joe", "Danger")
-    @guest_joe_danger_2 = Guest.new("Joe", "Danger")
+    @song_joe_elder = Song.new("Joe", "Elder")
+    @song_joe_danger_1 = Song.new("Joe", "Danger")
+    @song_joe_danger_2 = Song.new("Joe", "Danger")
 
-    @guest2_joe_danger = Guest2.new("Joe", "Danger")
+    @song2_joe_danger = Song2.new("Joe", "Danger")
   end
 
   # '==' tests
 
   def test_same_object_equals_is_true()
-    assert(@guest_joe_danger_1 == @guest_joe_danger_1)
+    assert(@song_joe_danger_1 == @song_joe_danger_1)
   end
 
   def test_different_object_same_variable_values_equals_is_true()
-    assert(@guest_joe_danger_1 == @guest_joe_danger_2)
+    assert(@song_joe_danger_1 == @song_joe_danger_2)
   end
 
   def test_different_varaiable_values_equals_is_false()
-    assert_equal(false, @guest_joe_elder == @guest_joe_danger_1)
+    assert_equal(false, @song_joe_elder == @song_joe_danger_1)
   end
 
   def test_different_class_equals_is_false()
-    assert_equal(false, @guest_joe_danger_1 == @guest2_joe_danger)
+    assert_equal(false, @song_joe_danger_1 == @song2_joe_danger)
   end
 
 
   # '!=' tests
 
   def test_same_object_not_equals_is_false()
-    assert_equal(false, @guest_joe_danger_1 != @guest_joe_danger_1)
+    assert_equal(false, @song_joe_danger_1 != @song_joe_danger_1)
   end
 
   def test_different_object_same_variable_values_not_equals_is_false()
-    assert_equal(false, @guest_joe_danger_1 != @guest_joe_danger_2)
+    assert_equal(false, @song_joe_danger_1 != @song_joe_danger_2)
   end
 
   def test_different_varaiable_values_not_equals_is_true()
-    assert(@guest_joe_elder != @guest_joe_danger_1)
+    assert(@song_joe_elder != @song_joe_danger_1)
   end
 
   def test_different_class_not_equals_is_true()
-    assert(@guest_joe_danger_1 != @guest2_joe_danger)
+    assert(@song_joe_danger_1 != @song2_joe_danger)
   end
 
 
   # array.include? tests
 
   def test_same_object_include_is_true()
-    assert([@guest_joe_danger_1].include?(@guest_joe_danger_1))
+    assert([@song_joe_danger_1].include?(@song_joe_danger_1))
   end
 
   def test_different_object_same_variable_values_include_is_true()
-    assert([@guest_joe_danger_1].include?(@guest_joe_danger_2))
+    assert([@song_joe_danger_1].include?(@song_joe_danger_2))
   end
 
   def test_different_varaiable_values_include_is_false()
-    assert_equal(false, [@guest_joe_elder].include?(@guest_joe_danger_1))
+    assert_equal(false, [@song_joe_elder].include?(@song_joe_danger_1))
   end
 
   def test_different_class_include_is_false()
-    assert_equal(false, [@guest_joe_danger_1].include?(@guest2_joe_danger))
+    assert_equal(false, [@song_joe_danger_1].include?(@song2_joe_danger))
   end
 
 end
