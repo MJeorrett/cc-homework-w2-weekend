018292e6f91ef222655fba416f198dc7715a0a03
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-07 13:40:10 +0100

    tests in record_spec tidied up

--- specs/record_spec.rb
+++ specs/record_spec.rb
@@ -3,6 +3,8 @@ require('minitest/rg')
 require_relative('../guest')
 require_relative('../guest2')
 
+require('pry-byebug')
+
 class RecordSpec < MiniTest::Test
 
   def setup()
@@ -13,20 +15,60 @@ class RecordSpec < MiniTest::Test
     @guest2_joe_danger = Guest2.new("Joe", "Danger")
   end
 
-  def test_same_object_matches()
+  # '==' tests
+
+  def test_same_object_equals_is_true()
     assert(@guest_joe_danger_1 == @guest_joe_danger_1)
   end
 
-  def different_object_same_values_matches()
+  def test_different_object_same_variable_values_equals_is_true()
     assert(@guest_joe_danger_1 == @guest_joe_danger_2)
   end
 
-  def test_different_varaible_values_doesnt_match()
+  def test_different_varaiable_values_equals_is_false()
     assert_equal(false, @guest_joe_elder == @guest_joe_danger_1)
   end
 
-  def test_different_class_doesnt_match()
+  def test_different_class_equals_is_false()
     assert_equal(false, @guest_joe_danger_1 == @guest2_joe_danger)
+  end
+
+
+  # '!=' tests
+
+  def test_same_object_not_equals_is_false()
+    assert_equal(false, @guest_joe_danger_1 != @guest_joe_danger_1)
+  end
+
+  def test_different_object_same_variable_values_not_equals_is_false()
+    assert_equal(false, @guest_joe_danger_1 != @guest_joe_danger_2)
+  end
+
+  def test_different_varaiable_values_not_equals_is_true()
+    assert(@guest_joe_elder != @guest_joe_danger_1)
+  end
+
+  def test_different_class_not_equals_is_true()
+    assert(@guest_joe_danger_1 != @guest2_joe_danger)
+  end
+
+
+  # array.include? tests
+
+  def test_same_object_include_is_true()
+    assert([@guest_joe_danger_1].include?(@guest_joe_danger_1))
+  end
+
+  def test_different_object_same_variable_values_include_is_true()
+    assert([@guest_joe_danger_1].include?(@guest_joe_danger_2))
+  end
+
+  def test_different_varaiable_values_include_is_false()
+    assert_equal(false, [@guest_joe_elder].include?(@guest_joe_danger_1))
+  end
+
+  def test_different_class_include_is_false()
+    assert_equal(false, [@guest_joe_danger_1].include?(@guest2_joe_danger))
   end
 
 end
