02ed1cc43ddde9af922fd99ae6cb67ace36b50d0
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-09 14:55:47 +0100

    updated record_spec to include lyrics in songs

--- specs/record_spec.rb
+++ specs/record_spec.rb
@@ -8,67 +8,67 @@ require('pry-byebug')
 class RecordTest < MiniTest::Test
 
   def setup()
-    @song_joe_elder = Song.new("Joe", "Elder", 1982)
-    @song_joe_danger_1 = Song.new("Joe", "Danger", 1999)
-    @song_joe_danger_2 = Song.new("Joe", "Danger", 1999)
+    @song_happy_birthday = Song.new("Happy Birthday", "Unknown", 1921, "Happy Birthday to you...")
+    @song_unhappy_birthday_1 = Song.new("Unhappy Birthday", "Unknown", 1921, "Happy Birthday to you...")
+    @song_unhappy_birthday_2 = Song.new("Unhappy Birthday", "Unknown", 1921, "Happy Birthday to you...")
 
-    @song2_joe_danger = Song2.new("Joe", "Danger", 1999)
+    @song2_unhappy_birthday = Song2.new("Unhappy Birthday", "Unknown", 1921, "Happy Birthday to you...")
   end
 
   # '==' tests
 
   def test_same_object_equals_is_true()
-    assert(@song_joe_danger_1 == @song_joe_danger_1)
+    assert(@song_unhappy_birthday_1 == @song_unhappy_birthday_1)
   end
 
   def test_different_object_same_variable_values_equals_is_true()
-    assert(@song_joe_danger_1 == @song_joe_danger_2)
+    assert(@song_unhappy_birthday_1 == @song_unhappy_birthday_2)
   end
 
   def test_different_varaiable_values_equals_is_false()
-    assert_equal(false, @song_joe_elder == @song_joe_danger_1)
+    assert_equal(false, @song_happy_birthday == @song_unhappy_birthday_1)
   end
 
   def test_different_class_equals_is_false()
-    assert_equal(false, @song_joe_danger_1 == @song2_joe_danger)
+    assert_equal(false, @song_unhappy_birthday_1 == @song2_unhappy_birthday)
   end
 
 
   # '!=' tests
 
   def test_same_object_not_equals_is_false()
-    assert_equal(false, @song_joe_danger_1 != @song_joe_danger_1)
+    assert_equal(false, @song_unhappy_birthday_1 != @song_unhappy_birthday_1)
   end
 
   def test_different_object_same_variable_values_not_equals_is_false()
-    assert_equal(false, @song_joe_danger_1 != @song_joe_danger_2)
+    assert_equal(false, @song_unhappy_birthday_1 != @song_unhappy_birthday_2)
   end
 
   def test_different_varaiable_values_not_equals_is_true()
-    assert(@song_joe_elder != @song_joe_danger_1)
+    assert(@song_happy_birthday != @song_unhappy_birthday_1)
   end
 
   def test_different_class_not_equals_is_true()
-    assert(@song_joe_danger_1 != @song2_joe_danger)
+    assert(@song_unhappy_birthday_1 != @song2_unhappy_birthday)
   end
 
 
   # array.include? tests
 
   def test_same_object_include_is_true()
-    assert([@song_joe_danger_1].include?(@song_joe_danger_1))
+    assert([@song_unhappy_birthday_1].include?(@song_unhappy_birthday_1))
   end
 
   def test_different_object_same_variable_values_include_is_true()
-    assert([@song_joe_danger_1].include?(@song_joe_danger_2))
+    assert([@song_unhappy_birthday_1].include?(@song_unhappy_birthday_2))
   end
 
   def test_different_varaiable_values_include_is_false()
-    assert_equal(false, [@song_joe_elder].include?(@song_joe_danger_1))
+    assert_equal(false, [@song_happy_birthday].include?(@song_unhappy_birthday_1))
   end
 
   def test_different_class_include_is_false()
-    assert_equal(false, [@song_joe_danger_1].include?(@song2_joe_danger))
+    assert_equal(false, [@song_unhappy_birthday_1].include?(@song2_unhappy_birthday))
   end
 
 end
--- testing_classes/song2.rb
+++ testing_classes/song2.rb
@@ -1,14 +1,15 @@
-require_relative('../record')
+require_relative('record')
 
 class Song2 < Record
 
-  attr_reader :title, :artist, :release_year
+  attr_reader :title, :artist, :release_year, :lyrics
 
-  def initialize(title, artist, release_year)
+  def initialize(title, artist, release_year, lyrics)
 
     @title = title
     @artist = artist
     @release_year = release_year
+    @lyrics = lyrics
 
   end
 
